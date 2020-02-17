5dfb9948cebe801abb4bf87d5f0ce0d2aec60de3
kaushiksamanta <kaushiksamanta23@gmail.com>
2020-02-16 19:04:38 -0500

    Fix: 🛠 Entities and migrations

---
+++ apps/api/src/migrations/1581884345869-url.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Url1581884345869 implements MigrationInterface {
+    name = 'url1581884345869'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`CREATE TABLE "counter" ("id" SERIAL NOT NULL, "count" integer NOT NULL, CONSTRAINT "PK_012f437b30fcf5a172841392ef3" PRIMARY KEY ("id"))`, undefined);
+        await queryRunner.query(`CREATE TABLE "url" ("id" SERIAL NOT NULL, "url" character varying NOT NULL, "shortUrl" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_7421088122ee64b55556dfc3a91" PRIMARY KEY ("id"))`, undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`DROP TABLE "url"`, undefined);
+        await queryRunner.query(`DROP TABLE "counter"`, undefined);
+    }
+
+}
