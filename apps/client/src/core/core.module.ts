f18ff339d7cca643d1591e501d8d3762996084a1
kaushiksamanta <kaushiksamanta23@gmail.com>
2020-02-23 20:17:04 -0500

    feat: Navbar component added

--- apps/client/src/core/core.module.ts
+++ apps/client/src/core/core.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from '../core/core.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    CoreModule
+    HttpClientModule
+  ],
+  exports: [
+    NavbarComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class CoreModule {}
