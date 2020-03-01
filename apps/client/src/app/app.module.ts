b63a70b56a75941bb1359bc60088e4456416c511
kaushiksamanta <kaushiksamanta23@gmail.com>
2020-02-29 22:51:51 -0500

    feat: Template driven form validation added

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from '../core/core.module';
@@ -8,6 +8,7 @@ import { CoreModule } from '../core/core.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule,
     CoreModule
