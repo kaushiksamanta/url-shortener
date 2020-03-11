72c2aeb67ddb29d29b90d2b8dcc652517eefaaaf
kaushiksamanta <kaushiksamanta23@gmail.com>
2020-03-10 23:39:47 -0400

    feat: Setup compose for development and production (https://github.com/kaushiksamanta/url-shortener/issues/4)

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -5,13 +5,23 @@
 ### ./docker-build-image.sh {api} {dev}
 #############################################################
 
-build_hash() {
+build_prod() {
   image="$1"
   git_short_hash=$(git rev-parse --short HEAD)
+  package_version=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
   echo "============================================================="
   echo "Building docker image :- url-shortener-$image:$git_short_hash"
   echo "============================================================="
-  docker build -t "url-shortener-$image:$git_short_hash" -f "$image.Dockerfile" .
+  docker build -t "url-shortener-$image:$git_short_hash" -t "url-shortener-$image:$package_version" -f "$image-prod.Dockerfile" .
+  echo "============================================================="
+  echo "Tagging docker image :- url-shortener-$image:$package_version"
+  echo "============================================================="
 }
 
 build_dev() {
@@ -30,6 +40,6 @@ else
   if [[ "$2" == "dev" ]];then
     build_dev $1 $2
   else
-    build_hash $1
+    build_prod $1
   fi
 fi
